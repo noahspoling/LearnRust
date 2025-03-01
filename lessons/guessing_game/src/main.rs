44a7a77c2a801f417d2206d16d5fc7912056799b
Noah Poling <noahspoling@gmail.com>
2025-03-01 17:32:24 -0500

    reordered loop for end game dialog and nested the tries loop.

--- lessons/guessing_game/src/main.rs
+++ lessons/guessing_game/src/main.rs
@@ -10,70 +10,81 @@ fn main() {
     println!("==========================================================");
 
     #[cfg(target_pointer_width = "64")]
-    type Int = u64;
+    type UInt = u64;
 
     #[cfg(target_pointer_width = "32")]
-    type Int = u32;
+    type UInt = u32;
 
     // Params
     let mut continue_game = true;
-    let mut tries : Int = 3;
-    let guess_range : Int = 10;
-    let mut guess_input = String::new();
-    let mut guess_number : Int = 0;
-    let mut secret_number : Int = 0;
-
-
-    // Setup
-    secret_number = rand::random::<Int>() % guess_range + 1;
-
-
+    let guess_range : UInt = 10;
+    
+    
     while continue_game {
 
-        println!("Guess the number! (1-{})", guess_range);
-        println!("You have {} tries left", tries);
-        tries -= 1;
+        let mut tries : UInt = 3;
+        let mut guess_input = String::new();
+        let mut guess_number : UInt = 0;
+        let mut secret_number : UInt = 0;
 
-        guess_input.clear();
+        // Setup
+        secret_number = rand::random::<UInt>() % guess_range + 1;
 
-        io::stdin()
-            .read_line(&mut guess_input)
-            .expect("Failed to read line");
+        while tries > 0 {
+            println!("Guess the number! (1-{})", guess_range);
+            println!("You have {} tries left", tries);
 
-        guess_number = match guess_input.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please enter a valid number!");
-                continue;
-            }
-        };
+            guess_input.clear();
 
-        println!("You guessed: {}", guess_input);
-        
-        match guess_number.cmp(&secret_number) {
-            Ordering::Less => { println!("Too small!"); }
-            Ordering::Greater => { println!("Too big!"); }
-            Ordering::Equal => {
-                println!("You win!");
-                tries = 3;
-                secret_number = rand::thread_rng().gen_range(1..guess_range);
-            }
-        };
-        
+            io::stdin()
+                .read_line(&mut guess_input)
+                .expect("Failed to read line");
+
+            guess_number = match guess_input.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Please enter a valid number!");
+                    continue;
+                }
+            };
+
+            println!("You guessed: {}", guess_input);
+            
+            match guess_number.cmp(&secret_number) {
+                Ordering::Less => { println!("Too small!"); }
+                Ordering::Greater => { println!("Too big!"); }
+                Ordering::Equal => {
+                    println!("You win!");
+                    break;
+                }
+            };
+            tries -= 1;
+        }
+
         if tries == 0 {
-            println!("Out of tries! Generating new secret number...");
-            tries = 3;
-            secret_number = rand::thread_rng().gen_range(1..guess_range);
+            println!("Out of tries! The secret number was: {}", secret_number);
         }
 
         let mut end_game = String::new();
-        println!("Do you want to play again? (y/n)");
-        io::stdin()
-            .read_line(&mut end_game)
-            .expect("Failed to read line");
+        loop {
+            println!("Do you want to play again? (y/n)");
+            io::stdin()
+                .read_line(&mut end_game)
+                .expect("Failed to read line");
 
-        if end_game.trim() == "n" || end_game.trim() == "N" {
-            continue_game = false;
+            match end_game.trim().to_lowercase().as_str() {
+                "y" => {
+                    continue_game = true;
+                    break;
+                }
+                "n" => {
+                    continue_game = false;
+                    break;
+                }
+                _ => {
+                    println!("Please enter 'y' or 'n'.");
+                }
+            }
         }
     }
 }
