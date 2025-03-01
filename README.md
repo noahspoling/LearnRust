eca6ec5f5fd93f53176a8da035c9f9cdda97b5b5
Noah Poling <noahspoling@gmail.com>
2025-02-28 19:51:59 -0500

    worked on guessing game

---
+++ README.md
@@ -0,0 +1,7 @@
+# Repository of the Topics in [The Rust Programming Book](https://doc.rust-lang.org/book/title-page.html)
+
+Can build and run projects in vscode 
+
+Ctrl + Shift + P 
+
+Tasks: Run Tasks
--- lessons/guessing_game/src/main.rs
+++ lessons/guessing_game/src/main.rs
@@ -1,3 +1,69 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("==========================================================");
+    println!("||                                                       ||");
+    println!("||                Welcome to Guessing Game               ||");
+    println!("||                                                       ||");
+    println!("==========================================================");
+
+    // Params
+    let mut continue_game = true;
+    let mut tries = 3;
+    let mut guess_range = 10;
+    let mut guess_input = String::new();
+    let mut guess_number = 0;
+    let mut secret_number = 0;
+
+    // Setup
+    secret_number = rand::random::<u32>() % guess_range + 1;
+
+
+    while continue_game {
+        if tries == 0 {
+            println!("Out of tries! Generating new secret number...");
+            tries = 3;
+            secret_number = rand::random::<u32>() % guess_range + 1;
+        }
+        println!("Guess the number! (1-{})", guess_range);
+        println!("You have {} tries left", tries);
+        tries -= 1;
+
+        guess_input.clear();
+
+        io::stdin()
+            .read_line(&mut guess_input)
+            .expect("Failed to read line");
+        
+        guess_number = match guess_input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess_input);
+        if guess_number < secret_number {
+            println!("Too small!");
+        } else if guess_number > secret_number {
+            println!("Too big!");
+        } else {
+            println!("You win!");
+            tries = 3;
+            secret_number = rand::random::<u32>() % 10 + 1;
+        }
+        
+
+        let mut end_game = String::new();
+        println!("Do you want to play again? (y/n)");
+        io::stdin()
+            .read_line(&mut end_game)
+            .expect("Failed to read line");
+
+        if end_game.trim() == "n" || end_game.trim() == "N" {
+            continue_game = false;
+        }
+    }
 }
